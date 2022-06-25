d45a3a691ecf7862598c14c447024788299adb6c
Haoming Xia <mrmist3r311@gmail.com>
2022-06-25 14:38:19 -0700

    frontend

--- backend/test/routes/root.test.ts
+++ backend/test/routes/root.test.ts
@@ -5,7 +5,8 @@ test('default root route', async (t) => {
   const app = await build(t)
 
   const res = await app.inject({
-    url: '/'
+    url: '/',
+    method: 'GET'
   })
-  t.same(JSON.parse(res.payload), { root: true })
+  t.equal(res.statusCode, 200)
 })
